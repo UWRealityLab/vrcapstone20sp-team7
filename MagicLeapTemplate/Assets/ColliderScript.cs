bd0e4e4e27b9980a98c0a0b0543de5424772d890
Kai-Wei Chang <kevin.chang.1998@outlook.com>
2020-05-04 23:16:09 -0700

    Added more detections on left hand

--- MagicLeapTemplate/Assets/ColliderScript.cs
+++ MagicLeapTemplate/Assets/ColliderScript.cs
@@ -12,31 +12,21 @@ using UnityEngine.UI;
 
 public class ColliderScript : MonoBehaviour
 {
-    public Text text;
+    public GameObject hand;
 
-    public GameObject marking, hand;
-
-    public Material invisible, material;
+    public Material material;
 
     private HashSet<string> processedMeshes = new HashSet<string>();
     private int index = 0;
 
+    public void CollisionEnter(Collision collision)
+    {
+        OnCollisionEnter(collision);
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collided");
-        //count += 1;
-        //Debug.Log("Collided, count = " + count);
-        //text.text = "count: " + count;
-        //if (collision.gameObject.name != "Plane" && collision.gameObject.name != "Plane(Clone)" && collision.gameObject.name != "Cylinder" && collision.gameObject.name != "Cylinder(Clone)")
-        //{
-        //    Debug.Log(collision.gameObject.name);
-        //    GameObject newPlane = Instantiate(marking);
-        //    //Debug.Log(collision.transform.rotation);
-        //    Vector3 pos = hand.transform.position;
-        //    pos.y -= (float)0.02;
-        //    newPlane.transform.position = pos;
-        //    // newPlane.transform.rotation = hand.transform.rotation;
-        //}
 
         MeshFilter meshFilter = collision.gameObject.GetComponent<MeshFilter>();
         Mesh mesh = meshFilter.sharedMesh;
@@ -59,44 +49,14 @@ public class ColliderScript : MonoBehaviour
 
                 GameObject meshFromSubmeshGameObject = new GameObject();
                 meshFromSubmeshGameObject.name = "SubMesh" + index;
-                //meshFromSubmeshGameObject.transform.SetParent(meshFilter.transform);
                 meshFromSubmeshGameObject.transform.localPosition = Vector3.zero;
                 meshFromSubmeshGameObject.transform.localRotation = Quaternion.identity;
                 MeshFilter meshFromSubmeshFilter = meshFromSubmeshGameObject.AddComponent<MeshFilter>();
                 meshFromSubmeshFilter.sharedMesh = newMesh;
-                //MeshRenderer meshFromSubmeshMeshRendererComponent = meshFromSubmeshGameObject.AddComponent<MeshRenderer>();
                 MeshCollider meshCollider = meshFromSubmeshGameObject.AddComponent<MeshCollider>();
                 meshCollider.sharedMesh = newMesh;
-                //Debug.Log(Resources.Load("Material/Red.mat", typeof(Material)) as Material);
-                //meshFromSubmeshMeshRendererComponent.material = invisible; //Resources.Load("Materials\\Red.mat", typeof(Material)) as Material;
                 index++;
             }
-
-            //for (int i = 0; i < mesh.subMeshCount; i++)
-            //{
-            //    int[] subMeshTriangles = mesh.GetTriangles(i);
-
-            //    Mesh newMesh = new Mesh();
-            //    newMesh.Clear();
-            //    newMesh.vertices = mesh.vertices;
-            //    newMesh.triangles = mesh.triangles;
-            //    newMesh.uv = mesh.uv;
-            //    newMesh.uv2 = mesh.uv2;
-            //    newMesh.uv2 = mesh.uv2;
-            //    newMesh.colors = mesh.colors;
-            //    newMesh.subMeshCount = mesh.subMeshCount;
-            //    newMesh.normals = mesh.normals;
-                
-            //    GameObject meshFromSubmeshGameObject = new GameObject();
-            //    meshFromSubmeshGameObject.name = "SubMesh" + count;
-            //    meshFromSubmeshGameObject.transform.SetParent(meshFilter.transform);
-            //    meshFromSubmeshGameObject.transform.localPosition = Vector3.zero;
-            //    meshFromSubmeshGameObject.transform.localRotation = Quaternion.identity;
-            //    MeshFilter meshFromSubmeshFilter = meshFromSubmeshGameObject.AddComponent<MeshFilter>();
-            //    meshFromSubmeshFilter.sharedMesh = CreateMesh(subMeshTriangles, i, mesh);
-            //    MeshRenderer meshFromSubmeshMeshRendererComponent = meshFromSubmeshGameObject.AddComponent<MeshRenderer>();
-            //    count++;
-            //}
         }
         else if (collision.gameObject.name.Contains("SubMesh"))
         {
@@ -108,88 +68,11 @@ public class ColliderScript : MonoBehaviour
             }
             meshFromSubmeshMeshRendererComponent.material = material;
         }
-        //else if (collision.gameObject.name.Contains("SubMesh"))
-        //{
-        //    Debug.Log("collided with submesh");
-        //}
-    }
-
-    private Mesh CreateMesh(int[] triangles, int index, Mesh mesh)
-    {
-        Mesh newMesh = new Mesh();
-        List<int> vertexIndices = new List<int>();
-        List<Vector3> verts = new List<Vector3>();
-        List<Color> colors = new List<Color>();
-        List<Vector3> normals = new List<Vector3>();
-
-        List<Vector2> uvs, uvs2, uvs3, uvs4;
-        uvs = new List<Vector2>(); uvs2 = new List<Vector2>(); uvs3 = new List<Vector2>(); uvs4 = new List<Vector2>();
-        List<int> tris = new List<int>();
-
-        newMesh.Clear();
-        int curVertIndex = 0;
-        int newVertIndex;
-        int curSubVertIndex = 0;
-        for (int i = 0; i < triangles.Length; i++)
-        {
-            curVertIndex = triangles[i];
-
-            if (!vertexIndices.Contains(curVertIndex))
-            {
-                newVertIndex = curSubVertIndex;
-                vertexIndices.Add(curVertIndex);
-
-                verts.Add(mesh.vertices[curVertIndex]);
-
-                if (mesh.colors != null && mesh.colors.Length > curVertIndex)
-                    colors.Add(mesh.colors[curVertIndex]);
-
-                normals.Add(mesh.normals[curVertIndex]);
-
-                if (mesh.uv != null && mesh.uv.Length > curVertIndex)
-                    uvs.Add(mesh.uv[curVertIndex]);
-                if (mesh.uv2 != null && mesh.uv2.Length > curVertIndex)
-                    uvs2.Add(mesh.uv2[curVertIndex]);
-                if (mesh.uv3 != null && mesh.uv3.Length > curVertIndex)
-                    uvs3.Add(mesh.uv3[curVertIndex]);
-                if (mesh.uv4 != null && mesh.uv4.Length > curVertIndex)
-                    uvs4.Add(mesh.uv4[curVertIndex]);
-
-                curSubVertIndex++;
-            }
-            else
-            {
-                newVertIndex = vertexIndices.IndexOf(curVertIndex);
-            }
-
-            tris.Add(newVertIndex);
-        }
-
-        newMesh.vertices = verts.ToArray();
-        newMesh.triangles = tris.ToArray();
-        if (uvs.Count > 0)
-            newMesh.uv = uvs.ToArray();
-        if (uvs2.Count > 0)
-            newMesh.uv2 = uvs2.ToArray();
-        if (uvs3.Count > 0)
-            newMesh.uv3 = uvs3.ToArray();
-        if (uvs4.Count > 0)
-            newMesh.uv4 = uvs4.ToArray();
-        if (colors.Count > 0)
-            newMesh.colors = colors.ToArray();
-
-        newMesh.Optimize();
-        newMesh.RecalculateBounds();
-        newMesh.RecalculateNormals();
-
-        return newMesh;
+        
     }
 
     void OnCollisionExit(Collision collision)
     {
-        //Debug.Log("Exit");
-        //count -= 1;
-        //Debug.Log("Exit, count = " + count);
-        //text.text = "count: " + count;
+        
     }
 }
---
+++ MagicLeapTemplate/Assets/ColliderSideScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderSideScript : MonoBehaviour
+{
+    public ColliderScript colliderScript;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        colliderScript.CollisionEnter(collision);
+    }
+}
